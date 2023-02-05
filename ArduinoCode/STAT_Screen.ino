dac0fba2a629d9a3a13d85c9522142e2e0bbdfbb
Guillem Companys Garcia <102296995+guillemcompanysgarcia@users.noreply.github.com>
2023-02-05 10:18:38 +0100

    V2

--- ArduinoCode/STAT_Screen.ino
+++ ArduinoCode/STAT_Screen.ino
@@ -1,112 +1,122 @@
-#include <UTFTGLUE.h>              //use GLUE class and constructor
+#include <UTFTGLUE.h>
 #include <string.h>
-UTFTGLUE myGLCD(0,A2,A1,A3,A4,A0); //initiate screen
 
-void setup()
-{
+#define UPDATE_INTERVAL 1000
+#define SLEEP_INTERVAL 10000
+#define UPPER_TEMP_TH 80
+#define LOWER_TEMP_TH 50
+
+
+#define GLCD_RS  0
+#define GLCD_WR  A2
+#define GLCD_CS  A1
+#define GLCD_RST A3
+#define GLCD_D0  A4
+#define GLCD_D1  A0
+
+UTFTGLUE myGLCD(GLCD_RS, GLCD_WR, GLCD_CS, GLCD_RST, GLCD_D0, GLCD_D1);
+
+int maxCpuTemp = 0;
+int maxGpuTemp = 0;
+
+unsigned long lastDataReceived = millis();
+
+bool display = false;
+
+
+void updateDisplay(){
+  myGLCD.InitLCD(0);
+  myGLCD.setFont(BigFont);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.fillRect(0, 0, 319, 479);
+  myGLCD.setBackColor(255, 255, 255);
+  myGLCD.setColor(0, 0, 0);
+
+
+  char *texts[] = {"CPU TEMP: ", "C", "CPU LOAD: ", "%", "GPU TEMP: ", "C", "GPU LOAD: ", "%"};
+  int x = LEFT, y = 30;
+
+  for (int i = 0; i < 8; i += 2) {
+    myGLCD.print(texts[i], x, y);
+    myGLCD.print(texts[i + 1], 300, y);
+    y += 135;
+  }
+
+  myGLCD.setFont(SevenSegNumFont);
+
+}
+void setup() {
   randomSeed(analogRead(0));
   Serial.begin(115200);
   Serial.setTimeout(1);
   
-  //Print the basic layout of the Display
-  myGLCD.InitLCD(0);
-  myGLCD.setFont(BigFont);
-  
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.fillRect(0, 0, 319, 479);
-  
-  myGLCD.setBackColor(255, 255, 255);
-  myGLCD.setColor(0, 0, 0);
-  
-  myGLCD.print("CPU TEMP: ", LEFT, 30);
-  myGLCD.print("C ",300, 30);
-
-  myGLCD.print("CPU LOAD: ", LEFT, 30+135);
-  myGLCD.print("%", 300, 30+135);
-  
-  myGLCD.print("GPU TEMP: ", LEFT, 30+135*2);
-  myGLCD.print("C", 300, 30+135*2);
-  
-  myGLCD.print("GPU LOAD: ", LEFT, 30+135*3);
-  myGLCD.print("%", 300, 30+135*3);
-
-  myGLCD.setFont(SevenSegNumFont);
-  
-  //End of the basic layout
+  updateDisplay();
 }
 
-void loop()
-{
-  String x= " ";
-
+void loop() {
+  String x = "";
   char str[20];
   char s[2] = ",";
   char *token;
   int aux[4];
 
-while (!Serial.available()); //Get data from the PC
+  while (!Serial.available()) {
+    unsigned long currentTime = millis();
+    if (currentTime - lastDataReceived >= SLEEP_INTERVAL) {
+      myGLCD.clrScr();
+      display = true;
+
+      maxCpuTemp = 0;
+      maxGpuTemp = 0;
+
+      return;
+    }
+  }
+  if (display) {
+    updateDisplay();
+    display = false;
+  }
   x = Serial.readString();
-  x.toCharArray(str,20);
-  token = strtok(str, s); //Conversions to split the string and get the data of each field.
-  int i=0;
-  while( token != NULL ) {
-  aux[i]=atoi(token);
-  i++;
-  token = strtok(NULL, s); 
- }
- 
- 
-  String cputemp, cpuload, gputemp, gpuload;
-  String bcputemp, bcpuload, bgputemp, bgpuload;
-  
-  bcputemp=cputemp;
-  cputemp=String(aux[0]);
-  if( bcputemp != cputemp){ //If the new read is different do a new print
-    if(aux[0]<10){ //Due to limitations of my screen I can't afford to clear the string so I print a "0" if the temp is lower than 10
-      cputemp="0"+cputemp;
-    }
-    if(aux[0]==100){ //Same but for 3 digits
-      cputemp="99";
-    }
-    myGLCD.print(cputemp, 200, 30-10);
-  }
-  
-  bcpuload=cpuload;
-  cpuload=String(aux[1]);
-  if( bcpuload != cpuload){
-    if(aux[1]<10){
-      cpuload="0"+cpuload;
-    }
-    if(aux[1]==100){
-      cpuload="99";
-    }
-    myGLCD.print(cpuload, 200, 30+135*1-10);
+  x.toCharArray(str, 20);
+  token = strtok(str, s);
+  int i = 0;
+  while (token != NULL) {
+    aux[i] = atoi(token);
+    i++;
+    token = strtok(NULL, s);
   }
 
-  bgputemp=gputemp;
-  gputemp=String(aux[2]);
-  if( bgputemp != gputemp){
-    if(aux[2]<10){
-      gputemp="0"+gputemp;
-    }
-    if(aux[2]==100){
-      gputemp="99";
-    }
-    myGLCD.print(gputemp, 200, 30+135*2-10);
-  }
-  
-  bgpuload=gpuload;
-  gpuload=String(aux[3]);
-  if( bgpuload != gpuload){
-    if(aux[3]<10){
-      gpuload="0"+gpuload;
-    }
-    if(aux[3]==100){
-      gpuload="99";
-    }
-    myGLCD.print(gpuload, 200, 30+135*3-10);
-  }
+  for (int j = 0; j < 4; j++) {
+    int value = aux[j];
+    if (j == 0) {
+      if (value > maxCpuTemp) {
+        maxCpuTemp = value;
+        myGLCD.setFont(BigFont);
+        myGLCD.setColor(0, 0, 0); // Black
+        myGLCD.print("Max CPU Temp: ", LEFT, 30 +(2 * j + 1)* 67);
+        myGLCD.print(String(maxCpuTemp), 200 + 16, 30 +(2 * j + 1)* 67);
+        myGLCD.setFont(SevenSegNumFont);
 
-  
-  delay (1000);
+      }
+      int color = (value >= LOWER_TEMP_TH) ? 255 : (value <= LOWER_TEMP_TH) ? 0 : map(value, LOWER_TEMP_TH, LOWER_TEMP_TH, 0, 255);
+      myGLCD.setColor(color, 255 - color, 0);
+    } else if (j == 2) {
+      if (value > maxGpuTemp) {
+        maxGpuTemp = value;
+        myGLCD.setFont(BigFont);
+        myGLCD.setColor(0, 0, 0); // Black
+        myGLCD.print("Max GPU Temp: ", LEFT, 30 +(2 * j + 1)* 67);
+        myGLCD.print(String(maxGpuTemp), 200 + 16, 30 +(2 * j + 1)* 67);
+        myGLCD.setFont(SevenSegNumFont);
+      }
+      int color = (value >= LOWER_TEMP_TH) ? 255 : (value <= LOWER_TEMP_TH) ? 0 : map(value, LOWER_TEMP_TH, LOWER_TEMP_TH, 0, 255);
+      myGLCD.setColor(color, 255 - color, 0);
+    } else {
+      myGLCD.setColor(0,0,0); 
+    }
+    String text = value == 100 ? "99" : value < 10 ? "0" + String(value) : String(value).substring(0, 2);
+    myGLCD.print(text, 200, 30 + j * 135 - 10);
+}
+  lastDataReceived = millis();
+  delay(UPDATE_INTERVAL);
 }
